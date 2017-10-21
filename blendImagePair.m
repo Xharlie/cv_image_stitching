3525267342997afc415b48528b60abf3e8e1e15c
xharlie <charlie.xu007@yahoo.com>
2017-10-20 21:43:12 -0800

    add blending functionality

--- blendImagePair.m
+++ blendImagePair.m
@@ -14,5 +14,22 @@ function out_img = blendImagePair(wrapped_imgs, masks, wrapped_imgd, maskd, mode
             + wrapped_imgd(:,:,3) .* (maskd ./ 255);
         imshow(out_img);
         hold on;
+    else
+        wrapped_imgs = single(wrapped_imgs);
+        wrapped_imgd = single(wrapped_imgd);
+        s_decay_mask = bwdist(s_mask) .* overlay_mask;
+        s_decay_mask = ((max(s_decay_mask(:)) - s_decay_mask) ...
+                            ./ max(s_decay_mask(:)) .* overlay_mask) + single(s_mask);
+        d_decay_mask = bwdist(d_mask) .* overlay_mask;
+        d_decay_mask = ((max(d_decay_mask(:)) - d_decay_mask) ...
+                            ./ max(d_decay_mask(:)) .* overlay_mask) + single(d_mask);   
+        
+        out_img(:,:,1) = (wrapped_imgs(:,:,1) .* s_decay_mask ...
+            + wrapped_imgd(:,:,1) .* d_decay_mask)./(s_decay_mask + d_decay_mask);
+        out_img(:,:,2) = (wrapped_imgs(:,:,2) .* s_decay_mask ...
+            + wrapped_imgd(:,:,2) .* d_decay_mask)./(s_decay_mask + d_decay_mask);
+        out_img(:,:,3) = (wrapped_imgs(:,:,3) .* s_decay_mask ...
+            + wrapped_imgd(:,:,3) .* d_decay_mask)./(s_decay_mask + d_decay_mask);
+        out_img = uint8(out_img);
     end
 end
